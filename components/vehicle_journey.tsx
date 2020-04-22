d0c522130dd17b63414b152d3bfab7cf4711ac46
Tristram Gräbener <tristram@midnighttrains.com>
2020-04-22 13:48:03 +0200

    Separate vehicle journey into component

---
+++ components/vehicle_journey.tsx
@@ -0,0 +1,33 @@
+import { fmt } from '../lib/helpers'
+import Segment from './segment'
+
+const VehicleJourney = ({ vj }) => <div className="p-12">
+<h1 className="text-xl font-bold">{vj.label}</h1>
+<div className="flex gap font-bold border border-gray-400 rounded m-1 px-1">
+  <div className="w-2/12">Segment</div>
+  <div className="w-1/12 text-right">Distance</div>
+  <div className="w-1/12 text-right">Énergie</div>
+  <div className="w-8/12 mx-6 flex">
+      <span className="w-1/12">€/km</span>
+      <span className="w-1/12">€/t-km</span>
+      <span className="w-1/12">€/kWh</span>
+      <span className="w-1/12">Total €</span>
+      <span className="w-6/12">Formule</span>
+  </div>
+  <div>Total</div>
+</div>
+<div>
+  {vj.segments.map((segment, index) => <Segment key={index} segment={segment}></Segment>)}
+</div>
+<div className="flex gap font-bold border border-gray-400 rounded m-1 px-1">
+  <div className="w-2/12">Totaux</div>
+  <div className="w-1/12 text-right">{vj.distance} km</div>
+  <div className="w-1/12 text-right">{vj.energy} kWh</div>
+  <div className="w-8/12 mx-6"></div>
+  <div className="flex"></div>
+  <div>{fmt(vj.price)} €</div>
+</div>
+</div>;
+
+
+export default VehicleJourney;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,11 @@
-import Segment from '../components/segment';
 import nord_east from '../lines/nord_east';
 import * as Trains from '../data/trains'
 import {vehicleJourney, fmt} from '../lib/helpers';
+import VehicleJourney from '../components/vehicle_journey';
 
 const vj = vehicleJourney(nord_east.segments, Trains.talgo230)
 
-const Home = ({ userAgent }) => <div className="p-12">
-  <h1 className="text-lg">{nord_east.label}</h1>
-  <div className="flex gap font-bold border border-gray-400 rounded m-1 px-1">
-    <div className="w-2/12">Segment</div>
-    <div className="w-1/12 text-right">Distance</div>
-    <div className="w-1/12 text-right">Énergie</div>
-    <div className="w-8/12 mx-6 flex">
-        <span className="w-1/12">€/km</span>
-        <span className="w-1/12">€/t-km</span>
-        <span className="w-1/12">€/kWh</span>
-        <span className="w-1/12">Total €</span>
-        <span className="w-6/12">Formule</span>
-    </div>
-    <div>Total</div>
-  </div>
-  <div>
-    {vj.segments.map((segment, index) => <Segment key={index} segment={segment}></Segment>)}
-  </div>
-  <div className="flex gap font-bold border border-gray-400 rounded m-1 px-1">
-    <div className="w-2/12">Totaux</div>
-    <div className="w-1/12 text-right">{vj.distance} km</div>
-    <div className="w-1/12 text-right">{vj.energy} kWh</div>
-    <div className="w-8/12 mx-6"></div>
-    <div className="flex"></div>
-    <div>{fmt(vj.price)} €</div>
-  </div>
-</div>;
+const Home = ({ userAgent }) => <VehicleJourney vj={vj}></VehicleJourney>;
 
 Home.getInitialProps = async ({ req }) => {
   const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
