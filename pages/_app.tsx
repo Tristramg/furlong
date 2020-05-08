7e6ed6eb21a0e8545a754db38fccdd8800208123
Tristram Gräbener <tristram@midnighttrains.com>
2020-05-08 11:17:06 +0200

    Add small icons for each rule category

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import { AppProps } from 'next/app';
 import '../styles/styles.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBolt, faBuilding, faRoad } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBolt, faBuilding, faRoad);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
