c00f7302a0e9c11b2e93183d92da1c5bd8a76c27
Tristram Gräbener <tristram@midnighttrains.com>
2020-05-14 11:48:10 +0200

    Use countries enum instead of strings

--- data/airtable_importer.ts
+++ data/airtable_importer.ts
@@ -1,6 +1,7 @@
 import { data } from './countries';
 import fetch from 'node-fetch';
 import { Line } from '../lib/types';
+import { Countries } from '../lib/types.d';
 import { edgeId } from '../lib/helpers';
 import _ from 'lodash';
 
@@ -16,29 +17,45 @@ async function get(offset: string, table: string) {
   return current;
 }
 
+const countriesMap = {
+  'IT': Countries.IT,
+  'BE': Countries.BE,
+  'FR': Countries.FR,
+  'DE': Countries.DE,
+  'ES': Countries.ES,
+  'PT': Countries.PT,
+}
+
 export default async function importAirtable() {
   const rawNodes = await get('', 'Nodes');
   const rawLines = await get('', 'Lines');
   const rawEdges = await get('', 'Edges');
   const id = n => edgeId(rawNodes[n.from[0]].Name, rawNodes[n.to[0]].Name);
 
-  const lines = _.mapValues(rawLines, l => ({
-    label: l.Name,
-    class: l.Class || null,
-    highSpeed: l.LGV || false,
-    gauge: l.Écartement || data[l.country].gauge,
-    signaling: l.Signalisation || data[l.country].signaling,
-    current: l.Courant || data[l.Country].current,
-  }));
+  const lines = _.mapValues(rawLines, l => {
+    const defaults = data[countriesMap[l.country]];
+    return {
+      label: l.Name,
+      class: l.Class || null,
+      highSpeed: l.LGV || false,
+      gauge: l.Écartement || defaults.gauge,
+      signaling: l.Signalisation || defaults.signaling,
+      current: l.Courant || defaults.current,
+    }
+  }
+  );
 
-  const defaultLine = (country: string): Line => ({
-    label: null,
-    class: null,
-    highSpeed: false,
-    gauge: data[country].gauge,
-    signaling: data[country].signaling,
-    current: data[country].current,
-  });
+  const defaultLine = (country: Countries): Line => {
+    const defaults = data[country];
+    return {
+      label: null,
+      class: null,
+      highSpeed: false,
+      gauge: defaults.gauge,
+      signaling: defaults.signaling,
+      current: defaults.current,
+    };
+  };
 
   return {
     props: {
@@ -47,10 +64,10 @@ export default async function importAirtable() {
         edges: _(rawEdges).values().map(v => [id(v), {
           departure: rawNodes[v.from[0]],
           arrival: rawNodes[v.to[0]],
-          country: v.Country,
+          country: countriesMap[v.Country],
           label: v.Line ? lines[v.Line[0]].label : '',
           distance: v.length,
-          line: v.Line ? lines[v.Line[0]] : defaultLine(v.Country),
+          line: v.Line ? lines[v.Line[0]] : defaultLine(countriesMap[v.Country]),
         }]).fromPairs().value(),
       },
     },
